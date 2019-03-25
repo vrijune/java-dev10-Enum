7d8ad9a143840a952a829dd3b620cb0e6b2712bc
Yi-Chien Vita Tsai <vita.tsai@auckland.ac.nz>
2019-03-26 11:41:05 +1300

    Industry Lab -- More collections and recursion

---
+++ src/ictgradschool/industry/recursion/ex04/ExerciseFour.java
@@ -0,0 +1,81 @@
+package ictgradschool.industry.recursion.ex04;
+
+/**
+ * Created by anhyd on 27/03/2017.
+ */
+public class ExerciseFour {
+
+    /**
+     * Returns the sum of all positive integers between 1 and num (inclusive).
+     */
+    public int getSum(int num) {
+
+        // TODO Implement a recursive solution to this method.
+        return 0;
+
+    }
+
+
+    /**
+     * Returns the smallest value in the given array, between the given first (inclusive) and second (exclusive) indices
+     *
+     * @param nums the array
+     * @param firstIndex the inclusive lower index
+     * @param secondIndex the exclusive upper index
+     */
+    public int getSmallest(int[] nums, int firstIndex, int secondIndex) {
+
+        // TODO Implement a recursive solution to this method.
+        return 0;
+    }
+
+    /**
+     * Prints all ints from n down to 1.
+     */
+    public void printNums1(int n) {
+
+        // TODO Implement a recursive solution to this method.
+        System.out.println("Not implemented.");
+    }
+
+    /**
+     * Prints all ints from 1 up to n.
+     */
+    public void printNums2(int n) {
+
+        // TODO Implement a recursive solution to this method.
+        System.out.println("Not implemented.");
+    }
+
+    /**
+     * Returns the number of 'e' and 'E' characters in the given String.
+     *
+     * @param input the string to check
+     */
+    public int countEs(String input) {
+
+        // TODO Implement a recursive solution to this method.
+        return 0;
+    }
+
+    /**
+     * Returns the nth number in the fibonacci sequence.
+     */
+    public int fibonacci(int n) {
+
+        // TODO Implement a recursive solution to this method.
+        return 0;
+    }
+
+    /**
+     * Returns true if the given input String is a palindrome, false otherwise.
+     *
+     * @param input the String to check
+     */
+    public boolean isPalindrome(String input) {
+
+        // TODO Implement a recursive solution to this method.
+        return false;
+    }
+
+}
